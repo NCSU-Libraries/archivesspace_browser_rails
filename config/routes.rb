46b0974ec14bd1625a54e8f727be52e887aa4f7d
Trevor Thornton <trthorn2@ncsu.edu>
2022-07-01 13:33:33 -0400

    support accessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-
+  
   get 'archivesspace_browser/api_get' => 'archivesspace_browser#api_get'
   get 'archivesspace_browser/resource_search/:q' => 'archivesspace_browser#resource_search'
   get 'archivesspace_browser/tree_root_data' => 'archivesspace_browser#get_tree_root_data'
   get 'archivesspace_browser/tree_node_data' => 'archivesspace_browser#get_tree_node_data'
   get 'archivesspace_browser/repositories/:repo_id/archival_objects/:id' => 'archivesspace_browser#get_archival_object'
-
+  get 'archivesspace_browser/*path', to: 'archivesspace_browser#api_get'
 end
